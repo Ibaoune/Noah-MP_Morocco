3feb063c4f8d5c93a351d942cadeacdac1bc18e5
M. El Aabaribaoune <mohammad.trop1@gmail.com>
2026-05-20 00:31:31 +0100

    Add HPC Toubkal configurations, header dependencies, and author comments

---
+++ lisf/lvt/make/LVT_misc.h
@@ -0,0 +1,12 @@
+#define USE_NETCDF4 
+#define USE_GRIBAPI 
+#define USE_ECCODES 
+#undef USE_HDF4 
+#undef USE_HDFEOS2 
+#define USE_HDF5 
+#undef AFWA_GRIB_CONFIGS 
+#undef USE_MATLAB_SUPPORT 
+#undef USE_GDAL 
+#undef INC_WATER_PTS
+#undef USE_GRIB2
+#undef SPMD
